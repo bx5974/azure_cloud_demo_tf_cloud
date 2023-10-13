d73cf40ba91eeb1676d53ee3378883edd32b73dd
bx5974 <bx5974@gmail.com>
2023-10-13 17:05:43 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "location" {
+  description = "location for the azure resources "
+}
+
+/*
+variable "vmpassword" {
+	description = "password for linux vm ssh login"
+}
+*/
+
+variable "ssh_port" {
+  description = "port number of ssh on vm"
+}
